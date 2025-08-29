236d486a1ea7165d7b91966d35459f37161d8b6f
valros76 <valerian.dufrene@gmail.com>
2025-08-29 13:32:24 +0200

    Advancement 29/08/25 13:32:22

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // On importe le module Express
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const Response = require("./models/Response"); // Importe le modèle Response, créé avec mongoose
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // On définit les fichiers statiques que le serveur a le droit de servir.
 app.use(express.static(path.join(__dirname, "public")));
+
+// On permet la lecture des données JSON dans les requêtes HTTP avec Express
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://webdevoopro_db_user:NextF0rmation42@cluster0.voyjctt.mongodb.net/")
+.then(
+  () => console.log("Connexion à la base MongoDB a réussi.")
+)
+.catch(
+  err => console.error("Une erreur s'est produite lors de la connexion à la base de données", err)
+);
 
 // On définit une route pour la page d'accueil
 // Lorsqu'un utilisateur va accéder à la racine ("/"), on va afficher "Hello World !"
@@ -16,8 +29,9 @@ app.get("/", (req, res) => {
 });
 
 // On ajoute une route API pour notre chatbot
-app.get("/api/chat", (req, res) => {
-  const userMessage = req.query.message ?? "Aucunes données fournies";
+app.get("/api/chat", async (req, res) => {
+  // On force le passage du message en minuscules pour faciliter la recherche.
+  const userMessage = req.body.message.toLowerCase() ?? "Aucunes données fournies";
   // Logique simple : "Bonjour" peut importe la question posée.
   res.json({
     text: `Bonjour ! Vous avez dit : ${userMessage}.`
@@ -40,6 +54,40 @@ app.get("/api/profile/:userId", (req, res) => {
   res.json({
     user: users[userId - 1]
   });
+});
+
+// On a besoin d'une route d'initialisation des données, qui ne sera a apeller qu'une seule fois.
+app.get("/api/init", async (req, res) => {
+  try{
+
+    // On supprime les questions et réponses existantes pour recommencer à utiliser le chatbot
+    await Response.deleteMany({});
+
+    // On ajoute les questions et réponses prédéfinies
+    await Response.insertMany([
+      {
+        question: "horaires",
+        answer: "Nous sommes ouvert de 09h00 à 18h00."
+      },
+      {
+        question: "retours",
+        answer: "Vous pouvez retourner un article dans les 30 jours suivant la date d'achat."
+      },
+      {
+        question: "contact",
+        answer: "Vous pouvez nous contacter par email à l'adresse test@mail.fr ou par téléphone au 06-01-02-03-04."
+      }
+    ]);
+
+    res.json({
+      message: "Les données ont été initialisée dans la base de données."
+    });
+
+  }catch(err){
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 // La dernière route à indiquer au serveur, c'est la gestion des routes non trouvées, soit l'erreur 404.
---
+++ models/Response.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// On definit notre shéma de données.
+// Une question de la part de l'utilisateur et une réponse de la part du chatbot.
+// Une question et une réponse, on a donc besoin du format "String", car ce sont des chaînes de caractères.
+const responseSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Response", responseSchema);
